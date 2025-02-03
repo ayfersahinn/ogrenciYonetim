625c6c01853ab8e16464dc2cbfa1a2720ab254f2
Ayfer Şahin <ayfers547@gmail.com>
2025-02-03 20:55:39 +0300

    ders silme tamamlandı

---
+++ dersSil.aspx.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace ogrenciYonetim
+{
+    public partial class dersSil : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            DataSet1TableAdapters.TBL_DERSTableAdapter dt = new DataSet1TableAdapters.TBL_DERSTableAdapter();
+            int id = Convert.ToInt32(Request.QueryString["DERSID"]);
+            dt.dersSil(id);
+            Response.Redirect("dersListesi.aspx");
+        }
+    }
+}
